3a2fe5981646350a1f0c57aaf5e7f206e71cf19e
Gabriel Ricardo Tubias Rocha <grtrocha@ucs.local>
2018-06-14 21:52:06 -0300

    Firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,36 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+//Firebase
+import { AngularFireModule, FirebaseAppConfig } from 'angularfire2';
+import { AngularFirestoreModule } from 'angularfire2/firestore';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+
+registerLocaleData(localePt, 'pt-BR');
+
+export const firebaseConfig: FirebaseAppConfig = {
+  apiKey: "AIzaSyBN818cbl2vBnbvchUNIcX7K_SFSNnHFDQ",
+  authDomain: "tcheoxeuai.firebaseapp.com",
+  databaseURL: "https://tcheoxeuai.firebaseio.com",
+  projectId: "tcheoxeuai",
+  storageBucket: "tcheoxeuai.appspot.com",
+  messagingSenderId: "518536022593"
+};
+
 @NgModule({
   declarations: [
     MyApp,
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule.enablePersistence(),
+    AngularFireAuthModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -21,7 +44,9 @@ import { MyApp } from './app.component';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    AngularFireAuth,
   ]
 })
 export class AppModule {}
