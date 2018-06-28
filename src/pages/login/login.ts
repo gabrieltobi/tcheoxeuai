e6010e2e3751987dc270b7c05ad0759525ecd328
Gabriel Ricardo Tubias Rocha <grtrocha@ucs.local>
2018-06-28 20:29:39 -0300

    Ajustes no login

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 import { AngularFireAuth } from 'angularfire2/auth';
+import firebase from 'firebase';
+import { AngularFirestore } from 'angularfire2/firestore';
 
 @IonicPage()
 @Component({
@@ -9,16 +11,24 @@ import { AngularFireAuth } from 'angularfire2/auth';
 })
 export class LoginPage {
 
-    constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth) {
+    constructor(public navCtrl: NavController, public navParams: NavParams, private afAuth: AngularFireAuth, private afs: AngularFirestore) {
     }
 
-    logar() {
+    entrarComFacebook() {
+        this.afAuth.auth.signInWithPopup(new firebase.auth.FacebookAuthProvider())
+            .then(userCredential => {
+                this.afs.collection<any>('pessoas').add({
+                    pessoa: 'Pessoa Teste',
+                    email: 'teste@gmail.com',
+                    uid: userCredential.user.uid
+                })
+            })
+    }
+
+    entrarComoConvidado() {
         this.afAuth
             .auth
             .signInWithEmailAndPassword('gabrieltubiass@gmail.com', '123456')
-            .then(value => {
-                //this.navCtrl.setRoot('HubPage');
-            })
             .catch(err => {
                 console.log('Something went wrong:', err.message);
             });
