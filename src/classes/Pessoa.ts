f0ff4b593c8a98beb40514d504e0efc68c2f45a2
root <thais.vergani1@gmail.com>
2018-06-28 22:07:43 -0300

    mostrar imagem na pergunta

--- src/classes/Pessoa.ts
+++ src/classes/Pessoa.ts
@@ -1,6 +1,10 @@
 import FirebaseItem from "./FirebaseItem";
+import Pergunta from "./Pergunta";
 
 export default class Pessoa extends FirebaseItem {
     email: string = ''
     pessoa: string = ''
+    // pergunta?: Pergunta = Pergunta()?
+    pergunta?: string = '1'
+
 }
--- src/pages/questao/questao.ts
+++ src/pages/questao/questao.ts
@@ -10,12 +10,12 @@ import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/fires
 export class QuestaoPage {
   pergunta: string = '123';
   alternativas: any[] = [];
-
+  imagem: string = "http://via.placeholder.com/350x150";
   constructor(public navCtrl: NavController, public navParams: NavParams, private afs: AngularFirestore) {
     let perguntas = afs.collection<any>('perguntas');
     perguntas.snapshotChanges().subscribe(data => {
       this.pergunta = data[0].payload.doc.data().pergunta;
-
+      this.imagem = data[0].payload.doc.data().imagem;
       afs.collection<any>('perguntas/' + data[0].payload.doc.id + '/alternativas').valueChanges().subscribe(data => {
         this.alternativas = data;
       })
@@ -24,9 +24,9 @@ export class QuestaoPage {
 
   responder(alternativa) {
     if (alternativa.correta) {
-      this.navCtrl.push('TransicaoPage', {'resposta':'Correta'});
+      this.navCtrl.push('TransicaoPage', {'resposta':'Resposta Correta!!'});
     } else {
-      this.navCtrl.push('TransicaoPage', {'resposta':'Incorreta'});
+      this.navCtrl.push('TransicaoPage', {'resposta':'Você errou :('});
     }
   }
 }
